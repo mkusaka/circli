39cfaf7b6e87ae71850a6963d2027779eaaa80db
mkusaka <hinoshita1992@gmail.com>
2022-05-01 11:44:06 +0900

    imliment cancel workflow cli

--- subCommands/workflow.ts
+++ subCommands/workflow.ts
@@ -1,6 +1,8 @@
 import { Command } from "../deps.ts";
 import { cancelRedundant } from "./workflow/cancelRedundant.ts";
+import { cancel } from "./workflow/cancel.ts";
 
 export const workflow = await new Command()
   .description("workflow command")
-  .command("cancel_redundant_workflow", cancelRedundant);
+  .command("cancel_redundant", cancelRedundant)
+  .command("cancel", cancel);
