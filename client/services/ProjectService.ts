2e3149fd76db294be550cdf5da45dbaf6693bfd4
GitHub Action <action@github.com>
2022-05-05 03:30:24 +0000

    daily update 20220505T033024

--- client/services/ProjectService.ts
+++ client/services/ProjectService.ts
@@ -211,41 +211,6 @@ export class ProjectService {
     });
   }
   /**
-   * List all environment variables
-   * Returns four 'x' characters, in addition to the last four ASCII characters of the value, consistent with the display of environment variable values on the CircleCI website.
-   * @returns any A sequence of environment variables.
-   * @throws ApiError
-   */
-  public static listEnvVars({
-    projectSlug,
-  }: {
-    /** Project slug in the form `vcs-slug/org-name/repo-name`. The `/` characters may be URL-escaped. **/
-    projectSlug: string;
-  }): CancelablePromise<{
-    items: Array<{
-      /**
-       * The name of the environment variable.
-       */
-      name: string;
-      /**
-       * The value of the environment variable.
-       */
-      value: string;
-    }>;
-    /**
-     * A token to pass as a `page-token` query parameter to return the next page of results.
-     */
-    next_page_token: string;
-  }> {
-    return __request(OpenAPI, {
-      method: "GET",
-      url: "/project/{project-slug}/envvar",
-      path: {
-        "project-slug": projectSlug,
-      },
-    });
-  }
-  /**
    * Create an environment variable
    * Creates a new environment variable.
    * @returns any Error response.
@@ -278,6 +243,41 @@ export class ProjectService {
       },
       body: requestBody,
       mediaType: "application/json",
+    });
+  }
+  /**
+   * List all environment variables
+   * Returns four 'x' characters, in addition to the last four ASCII characters of the value, consistent with the display of environment variable values on the CircleCI website.
+   * @returns any A sequence of environment variables.
+   * @throws ApiError
+   */
+  public static listEnvVars({
+    projectSlug,
+  }: {
+    /** Project slug in the form `vcs-slug/org-name/repo-name`. The `/` characters may be URL-escaped. **/
+    projectSlug: string;
+  }): CancelablePromise<{
+    items: Array<{
+      /**
+       * The name of the environment variable.
+       */
+      name: string;
+      /**
+       * The value of the environment variable.
+       */
+      value: string;
+    }>;
+    /**
+     * A token to pass as a `page-token` query parameter to return the next page of results.
+     */
+    next_page_token: string;
+  }> {
+    return __request(OpenAPI, {
+      method: "GET",
+      url: "/project/{project-slug}/envvar",
+      path: {
+        "project-slug": projectSlug,
+      },
     });
   }
   /**
