ed282a53837278da63aac1ce6275da78c70474dc
GitHub Action <action@github.com>
2022-08-11 00:03:00 +0000

    daily update 20220811T000300

--- client/services/WebhookService.ts
+++ client/services/WebhookService.ts
@@ -136,6 +136,32 @@ export class WebhookService {
     });
   }
   /**
+   * Delete a webhook
+   * @returns any A confirmation message
+   * @throws ApiError
+   */
+  public static deleteWebhook({
+    webhookId,
+  }: {
+    /**
+     * ID of the webhook (UUID)
+     */
+    webhookId: string;
+  }): CancelablePromise<{
+    /**
+     * A human-readable message
+     */
+    message: string;
+  }> {
+    return __request(OpenAPI, {
+      method: "DELETE",
+      url: "/webhook/{webhook-id}",
+      path: {
+        "webhook-id": webhookId,
+      },
+    });
+  }
+  /**
    * Get a webhook
    * Get a webhook by id.
    * @returns any A webhook
@@ -293,32 +319,6 @@ export class WebhookService {
       },
       body: requestBody,
       mediaType: "application/json",
-    });
-  }
-  /**
-   * Delete a webhook
-   * @returns any A confirmation message
-   * @throws ApiError
-   */
-  public static deleteWebhook({
-    webhookId,
-  }: {
-    /**
-     * ID of the webhook (UUID)
-     */
-    webhookId: string;
-  }): CancelablePromise<{
-    /**
-     * A human-readable message
-     */
-    message: string;
-  }> {
-    return __request(OpenAPI, {
-      method: "DELETE",
-      url: "/webhook/{webhook-id}",
-      path: {
-        "webhook-id": webhookId,
-      },
     });
   }
 }
