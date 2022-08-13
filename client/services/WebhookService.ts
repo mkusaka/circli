54b1fcadc9ce3a8ca319aea4b1f9213ed5bfc609
GitHub Action <action@github.com>
2022-08-13 00:03:19 +0000

    daily update 20220813T000319

--- client/services/WebhookService.ts
+++ client/services/WebhookService.ts
@@ -3,59 +3,6 @@ import { OpenAPI } from "../core/OpenAPI.ts";
 import { request as __request } from "../core/request.ts";
 export class WebhookService {
   /**
-   * Create a webhook
-   * @returns any Error response.
-   * @throws ApiError
-   */
-  public static createWebhook({
-    requestBody,
-  }: {
-    requestBody?: {
-      /**
-       * Name of the webhook
-       */
-      name: string;
-      /**
-       * Events that will trigger the webhook
-       */
-      events: Array<"workflow-completed" | "job-completed">;
-      /**
-       * URL to deliver the webhook to. Note: protocol must be included as well (only https is supported)
-       */
-      url: string;
-      /**
-       * Whether to enforce TLS certificate verification when delivering the webhook
-       */
-      "verify-tls": boolean;
-      /**
-       * Secret used to build an HMAC hash of the payload and passed as a header in the webhook request
-       */
-      "signing-secret": string;
-      /**
-       * The scope in which the relevant events that will trigger webhooks
-       */
-      scope: {
-        /**
-         * ID of the scope being used (at the moment, only project ID is supported)
-         */
-        id: string;
-        /**
-         * Type of the scope being used
-         */
-        type: "project";
-      };
-    };
-  }): CancelablePromise<{
-    message?: string;
-  }> {
-    return __request(OpenAPI, {
-      method: "POST",
-      url: "/webhook",
-      body: requestBody,
-      mediaType: "application/json",
-    });
-  }
-  /**
    * List webhooks
    * Get a list of webhook that match the given scope-type and scope-id
    * @returns any A list of webhooks
@@ -133,6 +80,151 @@ export class WebhookService {
         "scope-id": scopeId,
         "scope-type": scopeType,
       },
+    });
+  }
+  /**
+   * Create a webhook
+   * @returns any Error response.
+   * @throws ApiError
+   */
+  public static createWebhook({
+    requestBody,
+  }: {
+    requestBody?: {
+      /**
+       * Name of the webhook
+       */
+      name: string;
+      /**
+       * Events that will trigger the webhook
+       */
+      events: Array<"workflow-completed" | "job-completed">;
+      /**
+       * URL to deliver the webhook to. Note: protocol must be included as well (only https is supported)
+       */
+      url: string;
+      /**
+       * Whether to enforce TLS certificate verification when delivering the webhook
+       */
+      "verify-tls": boolean;
+      /**
+       * Secret used to build an HMAC hash of the payload and passed as a header in the webhook request
+       */
+      "signing-secret": string;
+      /**
+       * The scope in which the relevant events that will trigger webhooks
+       */
+      scope: {
+        /**
+         * ID of the scope being used (at the moment, only project ID is supported)
+         */
+        id: string;
+        /**
+         * Type of the scope being used
+         */
+        type: "project";
+      };
+    };
+  }): CancelablePromise<{
+    message?: string;
+  }> {
+    return __request(OpenAPI, {
+      method: "POST",
+      url: "/webhook",
+      body: requestBody,
+      mediaType: "application/json",
+    });
+  }
+  /**
+   * Update a webhook
+   * @returns any A webhook
+   * @throws ApiError
+   */
+  public static updateWebhook({
+    webhookId,
+    requestBody,
+  }: {
+    /**
+     * ID of the webhook (UUID)
+     */
+    webhookId: string;
+    requestBody?: {
+      /**
+       * Name of the webhook
+       */
+      name?: string;
+      /**
+       * Events that will trigger the webhook
+       */
+      events?: Array<"workflow-completed" | "job-completed">;
+      /**
+       * URL to deliver the webhook to. Note: protocol must be included as well (only https is supported)
+       */
+      url?: string;
+      /**
+       * Secret used to build an HMAC hash of the payload and passed as a header in the webhook request
+       */
+      "signing-secret"?: string;
+      /**
+       * Whether to enforce TLS certificate verification when delivering the webhook
+       */
+      "verify-tls"?: boolean;
+    };
+  }): CancelablePromise<{
+    /**
+     * URL to deliver the webhook to. Note: protocol must be included as well (only https is supported)
+     */
+    url: string;
+    /**
+     * Whether to enforce TLS certificate verification when delivering the webhook
+     */
+    "verify-tls": boolean;
+    /**
+     * The unique ID of the webhook
+     */
+    id: string;
+    /**
+     * Masked value of the secret used to build an HMAC hash of the payload and passed as a header in the webhook request
+     */
+    "signing-secret": string;
+    /**
+     * The date and time the webhook was last updated.
+     */
+    "updated-at": string;
+    /**
+     * Name of the webhook
+     */
+    name: string;
+    /**
+     * The date and time the webhook was created.
+     */
+    "created-at": string;
+    /**
+     * The scope in which the relevant events that will trigger webhooks
+     */
+    scope: {
+      /**
+       * ID of the scope being used (at the moment, only project ID is supported)
+       */
+      id: string;
+      /**
+       * Type of the scope being used
+       */
+      type: string;
+    };
+    /**
+     * Events that will trigger the webhook
+     */
+    events: Array<"workflow-completed" | "job-completed">;
+  }> {
+    return __request(OpenAPI, {
+      method: "PUT",
+      url: "/webhook/{webhook-id}",
+      path: {
+        "webhook-id": webhookId,
+      },
+      body: requestBody,
+      mediaType: "application/json",
     });
   }
   /**
@@ -227,98 +319,6 @@ export class WebhookService {
       path: {
         "webhook-id": webhookId,
       },
-    });
-  }
-  /**
-   * Update a webhook
-   * @returns any A webhook
-   * @throws ApiError
-   */
-  public static updateWebhook({
-    webhookId,
-    requestBody,
-  }: {
-    /**
-     * ID of the webhook (UUID)
-     */
-    webhookId: string;
-    requestBody?: {
-      /**
-       * Name of the webhook
-       */
-      name?: string;
-      /**
-       * Events that will trigger the webhook
-       */
-      events?: Array<"workflow-completed" | "job-completed">;
-      /**
-       * URL to deliver the webhook to. Note: protocol must be included as well (only https is supported)
-       */
-      url?: string;
-      /**
-       * Secret used to build an HMAC hash of the payload and passed as a header in the webhook request
-       */
-      "signing-secret"?: string;
-      /**
-       * Whether to enforce TLS certificate verification when delivering the webhook
-       */
-      "verify-tls"?: boolean;
-    };
-  }): CancelablePromise<{
-    /**
-     * URL to deliver the webhook to. Note: protocol must be included as well (only https is supported)
-     */
-    url: string;
-    /**
-     * Whether to enforce TLS certificate verification when delivering the webhook
-     */
-    "verify-tls": boolean;
-    /**
-     * The unique ID of the webhook
-     */
-    id: string;
-    /**
-     * Masked value of the secret used to build an HMAC hash of the payload and passed as a header in the webhook request
-     */
-    "signing-secret": string;
-    /**
-     * The date and time the webhook was last updated.
-     */
-    "updated-at": string;
-    /**
-     * Name of the webhook
-     */
-    name: string;
-    /**
-     * The date and time the webhook was created.
-     */
-    "created-at": string;
-    /**
-     * The scope in which the relevant events that will trigger webhooks
-     */
-    scope: {
-      /**
-       * ID of the scope being used (at the moment, only project ID is supported)
-       */
-      id: string;
-      /**
-       * Type of the scope being used
-       */
-      type: string;
-    };
-    /**
-     * Events that will trigger the webhook
-     */
-    events: Array<"workflow-completed" | "job-completed">;
-  }> {
-    return __request(OpenAPI, {
-      method: "PUT",
-      url: "/webhook/{webhook-id}",
-      path: {
-        "webhook-id": webhookId,
-      },
-      body: requestBody,
-      mediaType: "application/json",
     });
   }
 }
