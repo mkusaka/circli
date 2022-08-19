d8d5cdd826d60cbe405590bc7e47701ce2eaad6d
GitHub Action <action@github.com>
2022-08-19 00:03:58 +0000

    daily update 20220819T000358

--- client/services/ScheduleService.ts
+++ client/services/ScheduleService.ts
@@ -286,33 +286,6 @@ export class ScheduleService {
     });
   }
   /**
-   * Delete a schedule
-   * Deletes the schedule by id.
-   * @returns any A confirmation message.
-   * @throws ApiError
-   */
-  public static deleteScheduleById({
-    scheduleId,
-  }: {
-    /**
-     * The unique ID of the schedule.
-     */
-    scheduleId: string;
-  }): CancelablePromise<{
-    /**
-     * A human-readable message
-     */
-    message: string;
-  }> {
-    return __request(OpenAPI, {
-      method: "DELETE",
-      url: "/schedule/{schedule-id}",
-      path: {
-        "schedule-id": scheduleId,
-      },
-    });
-  }
-  /**
    * Update a schedule
    * Updates a schedule and returns the updated schedule.
    * @returns any A schedule object.
@@ -517,6 +490,33 @@ export class ScheduleService {
       },
       body: requestBody,
       mediaType: "application/json",
+    });
+  }
+  /**
+   * Delete a schedule
+   * Deletes the schedule by id.
+   * @returns any A confirmation message.
+   * @throws ApiError
+   */
+  public static deleteScheduleById({
+    scheduleId,
+  }: {
+    /**
+     * The unique ID of the schedule.
+     */
+    scheduleId: string;
+  }): CancelablePromise<{
+    /**
+     * A human-readable message
+     */
+    message: string;
+  }> {
+    return __request(OpenAPI, {
+      method: "DELETE",
+      url: "/schedule/{schedule-id}",
+      path: {
+        "schedule-id": scheduleId,
+      },
     });
   }
   /**
