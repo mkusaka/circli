2f03f0fb974d271b160eb0b9e3e5a4d6987af3b0
mkusaka <hinoshita1992@gmail.com>
2022-09-23 14:57:10 +0900

    sorter v2

--- sorter.ts
+++ sorter.ts
@@ -2,10 +2,25 @@ import { writeFileSync } from "fs";
 import swaggerJson from "./swagger";
 const filename = "swagger.json";
 function main() {
-  console.log(swaggerJson)
-  const dupSwaggerJson: typeof swaggerJson = JSON.parse(JSON.stringify(swaggerJson))
-  dupSwaggerJson.paths = Object.fromEntries(Object.entries(dupSwaggerJson.paths).sort(([key, _]) => key)) 
-  writeFileSync(filename, JSON.stringify(dupSwaggerJson, null, 2))
+  console.log(swaggerJson);
+  const dupSwaggerJson: typeof swaggerJson = JSON.parse(
+    JSON.stringify(swaggerJson)
+  );
+  dupSwaggerJson.paths = Object.fromEntries(
+    Object.entries(dupSwaggerJson.paths)
+      .map(([path, methods]) => {
+        return [
+          path,
+          Object.fromEntries(
+            Object.entries(methods).sort(([method, _]) => {
+              return method;
+            })
+          ),
+        ];
+      })
+      .sort(([path, _]) => path)
+  );
+  writeFileSync(filename, JSON.stringify(dupSwaggerJson, null, 2));
 }
 
-main()
+main();
