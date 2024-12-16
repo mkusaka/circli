dc8fde5684dbf0cba32d3311b33e2898ac95d233
mkusaka <hinoshita1992@gmail.com>
2024-12-16 15:21:57 +0900

    tidy up & generate command

--- client/models/pipeline.ts
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type pipeline = {
-  created_at?: string;
-  id?: string;
-  number?: number;
-  state?: "created" | "errored" | "setup-pending" | "setup" | "pending";
-};
