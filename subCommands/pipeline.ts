7718b44cea9b8615983824a8d946240fb807f226
mkusaka <hinoshita1992@gmail.com>
2025-02-08 17:59:06 +0900

    tidy up first

--- subCommands/pipeline.ts
+++
@@ -1,7 +0,0 @@
-import { Command } from "../deps.ts";
-import { cancel, name as cancelName } from "./pipeline/cancel.ts";
-
-export const name = "pipeline";
-export const pipeline = await new Command()
-  .description("pipeline command")
-  .command(cancelName, cancel);
